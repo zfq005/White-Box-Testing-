60135a3484dc03bf7dfa943019e6cbd17efee5e5
zfq005 <542675575@qq.com>
2019-11-19 12:43:54 +0800

    Version-1

--- src/main/java/com/test/jacoco/HelloWorld.java
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.test.jacoco;
 
+
 import javax.imageio.event.IIOReadWarningListener;
 
 
@@ -17,10 +18,10 @@ public class HelloWorld {
     }
     
     public int Method3(int a, int b, int c){
-    	if((a>5&&b<0)||c>0) {
+    	if((a>5&&b<0)||c>0){
     		System.out.println("Condition 1");
     	}
-    	else if(a<5 && c<-2 ) {
+    	else if(a<5 && c<-2 ){
     		System.out.println("Condition 2");
     	}
     	else {
@@ -34,7 +35,7 @@ public class HelloWorld {
     	}
     	int x = 0;
     	if((a == b) || ((c == d) && (bug(a)))) {
-    		 x = 1; 
+    		 x = 1;
     	}
     	e = 1/x;
     	return 0;
@@ -50,14 +51,41 @@ public class HelloWorld {
     	 * TODO: You need to complete this method to determine whether  a
     	 * triangle is formed or not when given the input edge a, b and c.
     	 */
-    	return true;
+		if(a+b>c&&a+c>b&&b+c>a)
+			return true;
+		else
+    		return false;
     }
     public boolean isBirthday(int year, int month, int day) {
     	/**
     	 * TODO: You need to complete this method to determine whether a 
     	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
-    	 * formed or not when given the input year, month and day.
+    	 * ormed or not when given the input year, month and day.
     	 */
+    	if(year<1990||month<1||day<1||year>2019||month>12)
+    		return false;
+    	if(year==2019){
+    		if(month==10&&day>1)
+    			return false;
+		}
+		if(month==2){
+			if(year%4==0){
+				if(day>29)
+					return false;
+			}
+			else{
+				if(day>28)
+					return false;
+			}
+		}
+		else if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+			if(day>31)
+				return false;
+		}
+		else{
+			if(day>30)
+				return false;
+		}
     	return true;
     	
     }
@@ -68,6 +96,20 @@ public class HelloWorld {
     	 * operation: "+","-","*","/". 
     	 */
     	double result = 0.0;
+    	if(op=='+')
+    		result=a+b;
+		else if(op=='-')
+			result=a-b;
+		else if(op=='*')
+			result=a*b;
+		else if(op=='/'){
+			if(b==0)
+				return result;
+			else
+				result=a/b;
+		}
+			else
+				return result;
     	return result;
     }
     
--- src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -31,9 +31,85 @@ public class HelloWorldTest {
     	 */
     	HelloWorld hw = new HelloWorld();
     	int a = hw.Method3(6, -1, 1);
-    	
+    	hw.Method3(6,1,1);
+    	hw.Method3(4,-1,1);
+    	hw.Method3(4,-1,-3);
+    	hw.Method3(4,-1,-2);
+    	hw.Method3(5,-1,-2);
     }
     /**
      * TODO: add the test function of other methods in HelloWorld.java
      */
+    @Test
+    public void testMethod4(){
+        HelloWorld hw=new HelloWorld();
+        try {
+            hw.Method4(0, 1, 1, 1, 1);
+
+            hw.Method4(4, 4, 1, 2, 1);
+            hw.Method4(5, 5, 1, 1, 1);
+            hw.Method4(4, 4, 1, 1, 1);
+            hw.Method4(5, 4, 1, 1, 1);
+            hw.Method4(4,3,1,1,1);
+            hw.Method4(5,4,1,2,1);
+            hw.Method4(4,3,1,2,1);
+        }catch (ArithmeticException e){
+            System.out.println("divide 0 error");
+        }
+    }
+    @Test
+    public void testTriangle(){
+        HelloWorld hw=new HelloWorld();
+        hw.isTriangle(1,1,1);
+        hw.isTriangle(2,2,1);
+        hw.isTriangle(3,4,5);
+        hw.isTriangle(1,1,2);
+        hw.isTriangle(1,3,1);
+        hw.isTriangle(3,1,1);
+    }
+    @Test
+    public void testBirthday(){
+        HelloWorld hw=new HelloWorld();
+        hw.isBirthday(1989,2,5);
+        hw.isBirthday(1990,0,5);
+        hw.isBirthday(1990,1,0);
+        hw.isBirthday(2020,1,5);
+        hw.isBirthday(2019,13,5);
+        hw.isBirthday(2019,10,5);
+        hw.isBirthday(2019,10,1);
+        hw.isBirthday(2019,9,5);
+        hw.isBirthday(2019,9,0);
+        hw.isBirthday(2016,2,30);
+        hw.isBirthday(2016,2,29);
+        hw.isBirthday(2017,2,29);
+        hw.isBirthday(2017,2,28);
+        hw.isBirthday(2019,1,5);
+        hw.isBirthday(2018,3,5);
+        hw.isBirthday(2018,5,5);
+        hw.isBirthday(2018,7,32);
+        hw.isBirthday(2018,8,31);
+        hw.isBirthday(2018,10,35);
+        hw.isBirthday(2018,12,45);
+        hw.isBirthday(2019,4,5);
+        hw.isBirthday(2018,6,30);
+        hw.isBirthday(2018,9,31);
+        hw.isBirthday(2018,11,32);
+    }
+
+    @Test
+    public void testCalculator(){
+        HelloWorld hw=new HelloWorld();
+        double a=hw.miniCalculator(1.0,2.0,'+');
+        //assertEquals(3.0, a);
+        a=hw.miniCalculator(2.0,1.0,'-');
+        //assertEquals(1.0, a);
+        a=hw.miniCalculator(1.0,2.0,'*');
+        //assertEquals(2.0, a);
+        a=hw.miniCalculator(1.0,2.0,'/');
+        //assertEquals(0.5, a);
+        a=hw.miniCalculator(1.0,0.0,'/');
+        //assertEquals(0, a);
+        a=hw.miniCalculator(1.0,2.0,'!');
+        //assertEquals(0, a);
+    }
 }
